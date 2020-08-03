6fd35b1d91594d28b9369a11d44357aafbbbb956
ssreshta0603 <ssreshta0603@gmail.com>
2020-08-03 19:46:13 +0530

    score and API calls

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var score = 0;
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +46,25 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("score "+ score, width-300,80);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +93,21 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    console.log(hour);
+    if(hour >= 06 && hour <= 18 ){
+        bg = "sprites/bg.png"
+    }
+    else{
+        
+        bg = "sprites/bg2.jpg";
+    }
+    backgroundImg = loadImage(bg);
+    
 }
